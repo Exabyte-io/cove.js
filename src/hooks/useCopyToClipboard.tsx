ff70e133bbfdd49214a49a5cb4eb128fa3c2b0a0
Oleksii Husakov <oleksii.husakov@exabyte.io>
2023-06-19 10:55:39 +0300

    chore: remove jquery in clipboard

--- src/hooks/useCopyToClipboard.tsx
+++ src/hooks/useCopyToClipboard.tsx
@@ -1,15 +1,17 @@
 import { useCallback } from "react";
 
 import { useAlert } from "../mui/components/custom/alert/hooks/useAlert";
+import { copyToClipboard } from "../utils/clipboard";
 
 export const useCopyToClipboard = (alertProps = {}) => {
     const alert = useAlert();
 
     return useCallback((text: string) => {
-        navigator.clipboard.writeText(text);
-        alert?.show({
-            content: `Unit flowchart id ${text} was successfully copied`,
-            ...alertProps,
+        copyToClipboard(text, () => {
+            alert?.show({
+                content: `Unit flowchart id ${text} was successfully copied`,
+                ...alertProps,
+            });
         });
     }, []);
 };
