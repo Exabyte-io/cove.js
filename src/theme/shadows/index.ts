fb38e60acd014c6c8e6abb769aef63af3bc04423
Sean Warren <seankwarren722@gmail.com>
2023-07-10 15:07:29 -0700

    feat: shadow added

--- src/theme/shadows/index.ts
+++ src/theme/shadows/index.ts
@@ -1,5 +1,7 @@
 import { Theme } from "@mui/material/styles";
 
-const Shadows = (theme: Theme) => theme.shadows;
+const Shadows = (theme: Theme) => {
+    return ["rgba(76, 78, 100, 0.22) 0px 2px 10px 0px", ...theme.shadows];
+};
 
 export default Shadows;
