8f57d81d81a903259fc70547b1ab4663f3451be7
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-18 12:01:55 -0700

    feat: add nested dropdown

---
+++ src/mui/components/nested-dropdown/NestedDropdownItem.tsx
@@ -0,0 +1,50 @@
+import Container from "@mui/material/Container";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import React, { useCallback } from "react";
+
+export interface DropdownItemProps {
+    disabled: boolean;
+    id: string;
+    onClick?: (id: string) => void;
+    isActive?: boolean;
+    leftIcon?: React.ReactElement;
+    content?: string;
+    rightIcon?: React.ReactElement;
+}
+
+/**
+ * @summary Nested dropdown item component. The main idea of the component is to provide
+ * flexible way to display leftIcon | content | rightIcon
+ * where each of them can be optional.
+ */
+export function NestedDropdownItem({
+    disabled = false,
+    id,
+    onClick,
+    isActive = false,
+    leftIcon,
+    content,
+    rightIcon,
+}: DropdownItemProps) {
+    const onItemClick = useCallback(() => {
+        if (typeof onClick === "function") {
+            onClick(id);
+        }
+    }, [id, onClick]);
+
+    return (
+        <MenuItem id={id} disabled={disabled} onClick={onItemClick}>
+            {Boolean(leftIcon) && <ListItemIcon>{leftIcon}</ListItemIcon>}
+            {Boolean(content) && (
+                <Container>
+                    <Typography variant="body1" color="text.primary" className="DropdownItemText">
+                        {content}
+                    </Typography>
+                </Container>
+            )}
+            {Boolean(rightIcon) && <ListItemIcon>{rightIcon}</ListItemIcon>}
+        </MenuItem>
+    );
+}
