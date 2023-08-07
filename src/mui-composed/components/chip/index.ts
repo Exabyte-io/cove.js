8c2e943206723e2d242392d70096d9cdbbef9e9b
Sean Warren <seankwarren722@gmail.com>
2023-08-07 11:43:55 -0700

    chore: index.ts added

---
+++ src/mui-composed/components/chip/index.ts
@@ -0,0 +1,6 @@
+import ChipList from "./ChipList";
+import ChipWithAction from "./ChipWithAction";
+import ChipWithDropdown from "./ChipWithDropdown";
+import ChipWithRJSFInput from "./ChipWithRJSFInput";
+
+export { ChipList, ChipWithAction, ChipWithDropdown, ChipWithRJSFInput };
