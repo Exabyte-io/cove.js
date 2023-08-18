73e405c2b4e9761df80cbefa1f6940f0d6ad4a7f
Oleksii Husakov <oleksii.husakov@exabyte.io>
2023-08-18 13:24:14 +0300

    chore: update types of DatePicker due to new DatePicker version

--- src/mui/components/custom/date-picker/DatePicker.tsx
+++ src/mui/components/custom/date-picker/DatePicker.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import Button from "@mui/material/Button";
-import { DateValidationError } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { PickerChangeHandlerContext } from "@mui/x-date-pickers/internals/hooks/usePicker/usePickerValue.types";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import classNames from "classnames";
 import moment, { Moment } from "moment";
@@ -206,10 +204,7 @@ function DatePicker({
                         className="date-from"
                         label="From"
                         // @ts-ignore
-                        onChange={(
-                            value: Moment,
-                            context: PickerChangeHandlerContext<DateValidationError>,
-                        ) => handleSelectDate("startDate", value, context)}
+                        onChange={(value, context) => handleSelectDate("startDate", value, context)}
                         value={selectedDateRage.startDate}
                         views={["year", "month", "day"]}
                     />
@@ -217,10 +212,7 @@ function DatePicker({
                         className="date-to"
                         label="To"
                         // @ts-ignore
-                        onChange={(
-                            value: Moment,
-                            context: PickerChangeHandlerContext<DateValidationError>,
-                        ) => handleSelectDate("endDate", value, context)}
+                        onChange={(value, context) => handleSelectDate("endDate", value, context)}
                         value={selectedDateRage.endDate}
                         views={["year", "month", "day"]}
                     />
