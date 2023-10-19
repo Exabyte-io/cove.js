d597037ac2070ba1b80b58f09b1a88104e2ac0c4
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2023-10-19 17:00:31 +0300

    chore: add dropdown button props

--- src/mui/components/dropdown/Dropdown.tsx
+++ src/mui/components/dropdown/Dropdown.tsx
@@ -42,7 +42,7 @@ export interface DropdownProps {
     paperPlacement?: PopperPlacementType;
     className?: string;
     disabled?: boolean;
-    buttonProps?: DefaultDropdownButtonProps;
+    buttonProps?: Partial<DefaultDropdownButtonProps>;
 }
 
 /**
