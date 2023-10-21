dfec1c3bdb0ed2adbcb814be7b230668229b8961
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-10-20 22:54:57 -0700

    update: adjust for new schema definitions

--- src/other/codemirror/utils/exaxyz_linter.ts
+++ src/other/codemirror/utils/exaxyz_linter.ts
@@ -2,16 +2,16 @@ import { Diagnostic } from "@codemirror/lint";
 import { Annotation, StateField, Transaction } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ConsistencyChecks } from "@exabyte-io/code.js/src/types";
+import { ConsistencyCheck } from "@exabyte-io/code.js/src/types";
 import _ from "underscore";
 
-export const ChecksAnnotation = Annotation.define<{ checks: ConsistencyChecks }>();
+export const ChecksAnnotation = Annotation.define<{ checks: Array<ConsistencyCheck> }>();
 
 export const checksStateField = StateField.define({
-    create(): ConsistencyChecks {
-        return { keys: [], messages: [] };
+    create(): Array<ConsistencyCheck> {
+        return [{ key: "", name: "default", message: "", severity: "error" }];
     },
-    update: (value: ConsistencyChecks, tr: Transaction) => {
+    update: (value: Array<ConsistencyCheck>, tr: Transaction) => {
         if (tr.annotation(ChecksAnnotation)) {
             // @ts-ignore
             return tr.annotation(ChecksAnnotation).checks;
@@ -23,12 +23,14 @@ const exaxyzLinter =
     () =>
     (view: EditorView): Diagnostic[] => {
         // TODO: REMOVE type casting
-        const checks: ConsistencyChecks = view.state.field(checksStateField) as ConsistencyChecks;
+        const checks: Array<ConsistencyCheck> = view.state.field(
+            checksStateField,
+        ) as Array<ConsistencyCheck>;
 
         if (!checks) return [];
         if (Object.keys(checks).length === 0) return [];
 
-        return checks.messages
+        return checks
             .map((check) => {
                 const keyFragments = check.key.split(".");
                 const atomIdStr = _.last(keyFragments);
