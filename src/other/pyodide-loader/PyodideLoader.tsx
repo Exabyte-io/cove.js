9eaaa20e8bf173d973dc2c7d2d9974770fa487d9
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-09 18:25:20 -0800

    update: add url for pyodide

--- src/other/pyodide-loader/PyodideLoader.tsx
+++ src/other/pyodide-loader/PyodideLoader.tsx
@@ -5,15 +5,25 @@ interface PyodideLoaderProps {
     children?: React.ReactNode;
 }
 
+interface PyodideLoaderState {
+    url: string;
+}
+
+const defaultUrl = "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js";
+
 // eslint-disable-next-line
-class PyodideLoader extends React.Component<PyodideLoaderProps> {
+class PyodideLoader extends React.Component<PyodideLoaderProps, PyodideLoaderState> {
     // eslint-disable-next-line
     constructor(props: PyodideLoaderProps) {
         super(props);
+        this.state = {
+            url: props.url || defaultUrl,
+        };
     }
 
     render() {
-        const { children, url } = this.props;
+        const { children } = this.props;
+        const { url } = this.state;
         return (
             <div>
                 <script src={url} />
