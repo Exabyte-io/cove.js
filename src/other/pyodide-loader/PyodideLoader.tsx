6a5ff64fd25595d56853660c50e2c2e38b13757a
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-10 16:12:34 -0800

    update: switch to functional component

--- src/other/pyodide-loader/PyodideLoader.tsx
+++ src/other/pyodide-loader/PyodideLoader.tsx
@@ -1,77 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import PyodideContext from "./PyodideContext";
 
 interface PyodideLoaderProps {
-    url: string;
-    children?: React.ReactNode;
+    url?: string;
+    children: React.ReactNode;
 }
 
-interface PyodideLoaderState {
-    url: string;
-    pyodideInitialized: boolean;
-    pyodide?: any;
-}
+const defaultSourceUrl = "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js";
+const importsUrl =
+    "https://raw.githubusercontent.com/Exabyte-io/api-examples/48f86e29c069fc0205216c50b1b98c19634a6445/other/pyodide/imports.py";
 
-const defaultUrl = "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js";
-
-class PyodideLoader extends React.Component<PyodideLoaderProps, PyodideLoaderState> {
-    constructor(props: PyodideLoaderProps) {
-        super(props);
-        this.state = {
-            url: props.url || defaultUrl,
-            pyodideInitialized: false,
-            pyodide: null,
-        };
-    }
-
-    componentDidMount(): void {
-        this.initializePyodide();
-    }
-
-    async initializePyodide() {
-        const { pyodide, pyodideInitialized, url } = this.state;
+function PyodideLoader({ url = defaultSourceUrl, children }: PyodideLoaderProps) {
+    const [pyodide, setPyodide] = useState<any>(null);
+    const [pyodideInitialized, setPyodideInitialized] = useState(false);
+    const initializePyodide = async () => {
         if (!pyodideInitialized) {
             const script =
-                document.querySelector(".pyodide-script") || document.createElement("script");
+                document.querySelector(".pyodide-script") ?? document.createElement("script");
             if (!document.querySelector(".pyodide-script")) {
                 // @ts-ignore
                 script.src = url;
                 script.className = "pyodide-script";
                 document.body.appendChild(script);
+
                 await new Promise<void>((resolve, reject) => {
                     // @ts-ignore
-                    script.onload = () => {
-                        this.setState({ pyodideInitialized: true });
-                        resolve();
-                    };
+                    script.onload = resolve;
                     // @ts-ignore
                     script.onerror = reject;
                 });
             }
 
-            // @ts-ignore
-            if (typeof window.loadPyodide === "undefined") {
-                throw new Error("loadPyodide is not available. Please check the CDN URL.");
-            }
+            const loadedPyodide = await (window as any).loadPyodide();
+            await loadedPyodide.loadPackage("micropip");
 
-            // @ts-ignore
-            this.setState({ pyodide: window.loadPyodide() });
-            await pyodide.loadPackage("micropip");
-
-            // TODO: add caching for our code as well
-            const response = await fetch(
-                "https://raw.githubusercontent.com/Exabyte-io/api-examples/48f86e29c069fc0205216c50b1b98c19634a6445/other/pyodide/imports.py",
-            );
+            const response = await fetch(importsUrl);
             const pythonCode = await response.text();
-            await pyodide.runPythonAsync(pythonCode);
+            await loadedPyodide.runPythonAsync(pythonCode);
 
-            // document.pyodideMplTarget = document.getElementById("pyodide-plot-target");
+            setPyodide(loadedPyodide);
+            setPyodideInitialized(true);
         }
+    };
+
+    useEffect(() => {
+        initializePyodide();
+    }, [url, pyodideInitialized]);
+
+    if (!pyodideInitialized) {
+        return <div>Loading Python Environment...</div>;
     }
 
-    render() {
-        const { children } = this.props;
-        return <div>{children}</div>;
-    }
+    return <PyodideContext.Provider value={pyodide}>{children}</PyodideContext.Provider>;
 }
 
 export default PyodideLoader;
