3a44ae319cde7cf151c2d80aa92b6f0d025d3db1
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-10 18:40:18 -0800

    chore: cleanup

--- src/other/pyodide-loader/PyodideLoader.tsx
+++ src/other/pyodide-loader/PyodideLoader.tsx
@@ -8,6 +8,12 @@ interface PyodideLoaderProps {
     triggerLoad?: boolean;
 }
 
+declare global {
+    interface Window {
+        loadPyodide: () => Promise<any>;
+    }
+}
+
 const defaultSourceUrl = "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js";
 const importsUrl =
     "https://raw.githubusercontent.com/Exabyte-io/api-examples/48f86e29c069fc0205216c50b1b98c19634a6445/other/pyodide/imports.py";
@@ -19,37 +25,39 @@ function PyodideLoader({
 }: PyodideLoaderProps) {
     const [pyodide, setPyodide] = useState<any>(null);
     const [pyodideInitialized, setPyodideInitialized] = useState(false);
-    const initializePyodide = async () => {
-        const script =
-            document.querySelector(".pyodide-script") ?? document.createElement("script");
-        if (!document.querySelector(".pyodide-script")) {
-            // @ts-ignore
-            script.src = url;
-            script.className = "pyodide-script";
+
+    const loadScript = (src: string) => {
+        return new Promise<void>((resolve, reject) => {
+            const script = document.createElement("script");
+            script.src = src;
+            script.onload = () => resolve();
+            script.onerror = () => reject(new Error(`Failed to load script ${src}`));
             document.body.appendChild(script);
+        });
+    };
 
-            await new Promise<void>((resolve, reject) => {
-                // @ts-ignore
-                script.onload = resolve;
-                // @ts-ignore
-                script.onerror = reject;
-            });
+    const initializePyodide = async () => {
+        try {
+            await loadScript(url);
+            const loadedPyodide = await window.loadPyodide();
+            await loadedPyodide.loadPackage("micropip");
+
+            const response = await fetch(importsUrl);
+            const pythonCode = await response.text();
+            await loadedPyodide.runPythonAsync(pythonCode);
+
+            setPyodide(loadedPyodide);
+            setPyodideInitialized(true);
+        } catch (error) {
+            console.error("Could not initialize Pyodide", error);
         }
-
-        const loadedPyodide = await (window as any).loadPyodide();
-        await loadedPyodide.loadPackage("micropip");
-
-        const response = await fetch(importsUrl);
-        const pythonCode = await response.text();
-        await loadedPyodide.runPythonAsync(pythonCode);
-
-        setPyodide(loadedPyodide);
-        setPyodideInitialized(true);
     };
 
     useEffect(() => {
-        if (triggerLoad && !pyodideInitialized) initializePyodide();
-    }, [url, pyodideInitialized, triggerLoad]);
+        if (triggerLoad && !pyodideInitialized) {
+            initializePyodide();
+        }
+    }, [triggerLoad, pyodideInitialized]);
 
     return <PyodideContext.Provider value={pyodide}>{children}</PyodideContext.Provider>;
 }
