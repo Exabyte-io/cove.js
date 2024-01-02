6fe3a32abd3b86f09338c91b5972877fa8eaa23c
Sean Warren <seankwarren722@gmail.com>
2024-01-02 10:28:17 -0800

    feat: made dismiss button default on snackbar alerts

--- src/theme/provider/AlertProvider.tsx
+++ src/theme/provider/AlertProvider.tsx
@@ -1,6 +1,9 @@
+import IconButton from "@mui/material/IconButton";
 import { makeStyles } from "@mui/styles";
-import { SnackbarProvider } from "notistack";
+import { closeSnackbar, SnackbarKey, SnackbarProvider } from "notistack";
 import React from "react";
+
+import IconByName from "../../mui/components/icon/IconByName";
 
 const useStyles = makeStyles({
     root: {
@@ -10,10 +13,17 @@ const useStyles = makeStyles({
 
 export function AlertProvider({ children }: { children: React.ReactNode }) {
     const classes = useStyles();
+
+    const closeButton = (snackbarId: SnackbarKey) => (
+        <IconButton onClick={() => closeSnackbar(snackbarId)}>
+            <IconByName name="actions.close" />
+        </IconButton>
+    );
     return (
         <SnackbarProvider
             maxSnack={3}
             autoHideDuration={4000}
+            action={closeButton}
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
             classes={{ containerRoot: classes.root }}>
             {children}
