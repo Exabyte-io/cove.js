799920882903dc5ceff8d9da05a6461e3d1c503a
Sean Warren <seankwarren722@gmail.com>
2024-01-02 17:32:00 -0800

    chore: types

--- src/theme/provider/AlertProvider.tsx
+++ src/theme/provider/AlertProvider.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import Alert from "@mui/material/Alert";
+import Alert, { AlertProps } from "@mui/material/Alert";
+import Button from "@mui/material/Button";
 import { makeStyles } from "@mui/styles";
 import { CustomContentProps, SnackbarContent, SnackbarProvider, useSnackbar } from "notistack";
 import React, { forwardRef } from "react";
@@ -10,16 +11,70 @@ const useStyles = makeStyles({
     },
 });
 
-const customAlert = forwardRef<HTMLDivElement, CustomContentProps>(
+declare module "notistack" {
+    interface VariantOverrides {
+        yesNo: {
+            onConfirm: () => void;
+            onDeny: () => void;
+            severity: AlertProps["severity"];
+        };
+    }
+}
+
+const customBasicAlert = forwardRef<HTMLDivElement, CustomContentProps>(
     ({ id, variant, ...props }, ref) => {
         const { closeSnackbar } = useSnackbar();
-        const mappedVariant = variant === "default" ? "info" : variant;
+        const mappedVariant = variant === "default" || variant === "yesNo" ? "info" : variant;
         return (
             <SnackbarContent ref={ref}>
                 <Alert
                     severity={mappedVariant}
                     className={`alert alert-${mappedVariant}`}
                     onClose={() => closeSnackbar(id)}
+                    variant="filled">
+                    {props.message}
+                </Alert>
+            </SnackbarContent>
+        );
+    },
+);
+
+interface YesNoAlertProps extends CustomContentProps {
+    onConfirm: () => void;
+    onDeny: () => void;
+    severity: AlertProps["severity"];
+}
+
+const customYesNoAlert = forwardRef<HTMLDivElement, YesNoAlertProps>(
+    ({ id, severity, onConfirm, onDeny, ...props }, ref) => {
+        const { closeSnackbar } = useSnackbar();
+        console.log({ id, severity, onConfirm });
+        return (
+            <SnackbarContent ref={ref}>
+                <Alert
+                    severity={severity}
+                    className={`alert alert-${severity}`}
+                    onClose={() => closeSnackbar(id)}
+                    action={
+                        <>
+                            <Button
+                                color={severity}
+                                onClick={() => {
+                                    onConfirm();
+                                    closeSnackbar();
+                                }}>
+                                Yes
+                            </Button>
+                            <Button
+                                color={severity}
+                                onClick={() => {
+                                    onDeny();
+                                    closeSnackbar();
+                                }}>
+                                No
+                            </Button>
+                        </>
+                    }
                     variant="filled">
                     {props.message}
                 </Alert>
@@ -36,11 +91,12 @@ export function AlertProvider({ children }: { children: React.ReactNode }) {
             maxSnack={3}
             autoHideDuration={4000}
             Components={{
-                error: customAlert,
-                warning: customAlert,
-                info: customAlert,
-                success: customAlert,
-                default: customAlert,
+                error: customBasicAlert,
+                warning: customBasicAlert,
+                info: customBasicAlert,
+                success: customBasicAlert,
+                default: customBasicAlert,
+                yesNo: customYesNoAlert,
             }}
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
             classes={{ containerRoot: classes.root }}>
