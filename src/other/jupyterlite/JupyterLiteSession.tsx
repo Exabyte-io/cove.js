b2016e8f7ffec3e492a16baaf4babcc081523f2f
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-03-12 13:38:43 -0700

    chore: actually export component

--- src/other/jupyterlite/JupyterLiteSession.tsx
+++ src/other/jupyterlite/JupyterLiteSession.tsx
@@ -12,7 +12,7 @@ const JupyterLiteSessionDefaultProps: JupyterLiteSessionProps = {
     defaultNotebookPath: null,
     frameId: "jupyter-lite-iframe",
     onMessage: () => {},
-}
+};
 
 class JupyterLiteSession extends React.Component<JupyterLiteSessionProps> {
     constructor(props: JupyterLiteSessionProps = JupyterLiteSessionDefaultProps) {
@@ -20,11 +20,11 @@ class JupyterLiteSession extends React.Component<JupyterLiteSessionProps> {
     }
 
     componentDidMount() {
-        window.addEventListener('message', this.handleReceiveMessage, false);
+        window.addEventListener("message", this.handleReceiveMessage, false);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('message', this.handleReceiveMessage, false);
+        window.removeEventListener("message", this.handleReceiveMessage, false);
     }
 
     handleReceiveMessage = (event: MessageEvent) => {
@@ -33,7 +33,7 @@ class JupyterLiteSession extends React.Component<JupyterLiteSessionProps> {
     };
 
     sendDataToIFrame = (data: Record<string, unknown>[], variableName = "data") => {
-        const message = {type: "from-host-to-iframe", data, variableName};
+        const message = { type: "from-host-to-iframe", data, variableName };
         const iframe = document.getElementById(this.props.frameId) as HTMLIFrameElement | null;
         if (iframe && iframe.contentWindow) {
             iframe.contentWindow.postMessage(message, this.props.originURL);
@@ -43,7 +43,9 @@ class JupyterLiteSession extends React.Component<JupyterLiteSessionProps> {
     };
 
     render() {
-        const src = this.props.defaultNotebookPath ? `${this.props.originURL}/lab/tree?path=${this.props.defaultNotebookPath}` : `${this.props.originURL}/lab`;
+        const src = this.props.defaultNotebookPath
+            ? `${this.props.originURL}/lab/tree?path=${this.props.defaultNotebookPath}`
+            : `${this.props.originURL}/lab`;
         return (
             <iframe
                 name="jupyterlite"
@@ -57,3 +59,5 @@ class JupyterLiteSession extends React.Component<JupyterLiteSessionProps> {
         );
     }
 }
+
+export default JupyterLiteSession;
---
+++ src/other/jupyterlite/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./JupyterLiteSession";
+export * from "./JupyterLiteSession";
