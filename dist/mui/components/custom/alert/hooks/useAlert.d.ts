cd979958966619bab1904abb6bb5234a21f3fdbf
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-03 19:59:23 -0700

    update: run tsc

--- dist/mui/components/custom/alert/hooks/useAlert.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const useAlert: () => {
-    show: (alertProps: import("../AlertContextProvider").ExtendedAlertProps, snackbarProps?: import("@mui/material").SnackbarProps | undefined) => void;
-} | null;
