97c8339d523b0258cd1b750e8cb137de98c21dfe
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-07-24 21:35:09 +0300

    chore: remove containers

--- src/mui/components/containers/ColumnContainer.tsx
+++
@@ -1,8 +0,0 @@
-import Stack, { StackProps } from "@mui/material/Stack";
-import React from "react";
-
-export type ColumnContainerProps = StackProps;
-
-export default function ColumnContainer({ children }: ColumnContainerProps) {
-    return <Stack overflow="hidden">{children}</Stack>;
-}
--- src/mui/components/containers/ScrollableContainer.tsx
+++
@@ -1,8 +0,0 @@
-import Box, { BoxProps } from "@mui/material/Box";
-import React from "react";
-
-export type ScrollableContainerProps = BoxProps;
-
-export default function ScrollableContainer({ children }: ScrollableContainerProps) {
-    return <Box overflow="auto">{children}</Box>;
-}
